3bbf707ce8ff11550fe155c20f2c3e7ba442262e
Chethan <padma.chethan@gmail.com>
2025-03-14 00:33:47 +0530

    Added health endpoint

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -18,6 +19,19 @@ func main() {
 	}
 
 	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1router := chi.NewRouter()
+	v1router.Get("/health", handlerReadiness)
+	router.Mount("/v1", v1router)
 
 	srv := &http.Server{
 		Handler: router,
